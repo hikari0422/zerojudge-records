f289b4ddf54b547bbc18e38e60f44d4faae2e480
noahsarktaiwan <ben109092ben@gmail.com>
2024-12-08 17:36:58 +0800

    update

---
+++ a001/a001.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char input[1000];
+    scanf("%s", input);
+    printf("hello, %s", input);
+}
---
+++ a001/a001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class a001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String input = scanner.nextLine();
+        System.out.println("hello, " + input);
+
+        scanner.close();
+    }
+}
